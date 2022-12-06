5d471d6a046cd7a36429f9d9cd68a94dc35d0094
govind singh <118297088+Govind-768@users.noreply.github.com>
2022-12-06 13:42:32 +0530

    Add files via upload

---
+++ calculator mini project.py
@@ -0,0 +1,15 @@
+num=int(input("Enter the number you want to check\n"))
+temp=1
+k=0
+a=0
+summ=0
+while summ<=num:
+    summ=temp+k
+    temp=summ
+    k=temp
+    if summ==num:
+        a=a+1
+        print("Yes. {} is a fibonnaci number".format(num))
+        break
+if a==0:
+    print("No. {} is NOT a fibonacci number".format(num))
